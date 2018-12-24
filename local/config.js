64be41bac4a1e8f53e11eb3c5d7b5bab5409492a
Blaž Beuermann <blaz.be@gmail.com>
2018-12-24 23:31:22 +0100

    A flash of local files (ignored by .gitignore)

---
+++ local/config.js
@@ -0,0 +1,8 @@
+export const AppConfig = {
+	brand: 'In development',
+	meta: {
+		defaultTitle: 'React.js Boilerplate',
+		titleTemplate: '%s - React.js Boilerplate',
+		description: 'A React.js Boilerplate application'
+	}
+}
